fc4309ca24787e67b056db8b0ce6b23cb21847a3
Uday <udayomsrivastava111@gmail.com>
2024-03-03 11:18:51 +0530

    hosting

--- client/src/Pages/CreatePost.jsx
+++ client/src/Pages/CreatePost.jsx
@@ -28,7 +28,11 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle/', {
+
+        // the below line can be uncommented if you are running the server locally
+
+        // const response = await fetch('http://localhost:8080/api/v1/dalle/', {
+        const response = await fetch('https://dall-e-2-0-ai-image-generation-website.onrender.com/api/v1/dalle/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -58,7 +62,10 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        // the below line can be uncommented if you are running the server locally
+        
+        // const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dall-e-2-0-ai-image-generation-website.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
